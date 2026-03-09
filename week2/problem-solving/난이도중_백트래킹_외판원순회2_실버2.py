31a7865ba36f144a206393c2d2399755044b8096
남동현 <ehdgus5178@gmail.com>
2026-03-09 20:39:16 +0900

    백준 10971번 해결

--- week2/problem-solving/난이도중_백트래킹_외판원순회2_실버2.py
+++ week2/problem-solving/난이도중_백트래킹_외판원순회2_실버2.py
@@ -1,2 +1,39 @@
-# 백트래킹 - 외판원 순회 2 (백준 실버2)
-# 문제 링크: https://www.acmicpc.net/problem/10971
+import random
+
+n = int(input())
+
+city = []
+for i in range(n):
+    row = []
+    for j in range(n):
+        row.append(random.randrange(1, 10))
+    city.append(row)
+
+for i in range(n):
+    city[i][i] = 0
+
+answer = 1000000000
+
+def backtrack(start, current_city, visited_city, cost, count):
+    global answer
+
+    if count == n and city[current_city][start] != 0:
+        total_cost = cost + city[current_city][start]
+        if total_cost < answer:
+            answer = total_cost
+            return
+
+    for next_city in range(n):
+        if visited_city[next_city] == False and city[current_city][next_city] != 0:
+            visited_city[next_city] = True
+            backtrack(start, next_city, visited_city, cost + city[current_city][next_city], count + 1)
+            visited_city[next_city] = False
+
+for i in range(n):
+    print(city[i])
+
+visited_city = [False] * n
+visited_city[0] = True
+
+backtrack(0, 0, visited_city, 0, 1)
+print("최소 비용:", answer)
