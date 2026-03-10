54f68b071682fccf2925ceb705ddd43f879acfd3
남동현 <ehdgus5178@gmail.com>
2026-03-10 16:34:44 +0900

    백준 문자열 반복 해결
    난 이때까지 입력에 모든 입력을 받고 출력창을 한번만 나오게 하는줄 알았는데 그게 아니었다

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,19 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+ipnum=int(input())
+r=""
+s=""
+for i in range(ipnum):
+    r,s=input().split(" ")
+    r=int(r)
+
+    for j in s:
+        print(j*r,end="")
+    print()
+    r=""
+    s=""
+
+
+
+
