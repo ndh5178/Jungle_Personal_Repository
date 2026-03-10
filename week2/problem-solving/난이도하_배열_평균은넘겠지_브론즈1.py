9463508b1874bf89dc607627c0858a47636a0220
남동현 <ehdgus5178@gmail.com>
2026-03-10 16:08:00 +0900

    백준 평균은 넘겠지 해결

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,27 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+ipnum=int(input())
+
+stdnum=[]
+
+sumnum=0
+abnum=0
+abupstd=0
+for i in range(ipnum):
+    stdnum.append(list(map(int,input().split(" "))))
+
+for i in range(ipnum):
+    for j in range(len(stdnum[i])):
+        sumnum=sum(stdnum[i])-stdnum[i][0]
+        
+for i in range(ipnum):
+    sumnum=sum(stdnum[i])-stdnum[i][0]
+    abnum=sumnum/stdnum[i][0]
+    for j in range(1,len(stdnum[i])):
+        if stdnum[i][j]>abnum:
+            abupstd+=1
+    print(str(round((abupstd/stdnum[i][0])*100,3))+"%")
+    abupstd=0
+
+
