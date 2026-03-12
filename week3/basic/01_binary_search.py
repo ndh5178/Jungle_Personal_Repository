3ad8c95df7e4f34b5256f6d42ab90722903196f4
남동현 <ehdgus5178@gmail.com>
2026-03-12 20:50:27 +0900

    3week의 파일수정 밎 이분탐색 해결

--- week3/basic/01_binary_search.py
+++ week3/basic/01_binary_search.py
@@ -33,15 +33,26 @@ def binary_search(arr, target):
     Returns:
         target의 인덱스 (없으면 -1)
     """
+
     left = 0
     right = len(arr) - 1
     
     # TODO: left가 right보다 작거나 같을 때까지 반복
+
+
     ## 중간 인덱스 계산
     ## arr[mid]와 target 비교
     ## 같으면 mid 반환
     ## target이 더 크면 left = mid + 1
     ## target이 더 작으면 right = mid - 1
+    while left<=right:
+        mid = (left + right) // 2
+        if arr[mid]==target:
+            return mid
+        if target>arr[mid]:
+            left = mid + 1
+        if target<arr[mid]:
+            right = mid -1
     pass
     
     return -1
