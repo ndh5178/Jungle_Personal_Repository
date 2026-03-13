d387f73aa56137da731b9a8067489545b8dbf28b
남동현 <ehdgus5178@gmail.com>
2026-03-13 20:50:54 +0900

    백준 스택 해결
    시간 초과가 나와서 시간을 단축 하려면
    import sys
    input = sys.stdin.readline
    이거 꼭 써줘야 겠다

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,47 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+import sys
+input = sys.stdin.readline
+n=int(input())
+
+astack=[]
+bstack=[]
+
+def push(num):
+    astack.append(num)
+def pop():
+    if not astack:
+        return -1
+    return astack.pop()
+def size():
+    return len(astack)
+def empty():
+    if len(astack)==0:
+        return 1
+    else:
+        return 0
+def top():
+    if len(astack)==0:
+        return -1
+    x=len(astack)
+    return astack[x-1]
+
+
+for i in range(n):
+    string=input().split()
+    if len(string)==2:
+        x=string.pop()
+    y=string.pop()
+    if y=="push":
+        push(int(x))
+    if y=="pop":
+        print(pop())
+    if y=="size":
+        print(size())
+    if y=="empty":
+        print(empty())
+    if y=="top":
+        print(top())
+
+
+
