31aa4e58ff9009a58ea5556ecd15f651cc4e5080
남동현 <ehdgus5178@gmail.com>
2026-03-13 17:54:53 +0900

    민균이 비밀번호 해결
    처음에는 스택으로 풀어서 시간복잡도가 n제곱 나왔는데 해시테이블로 푸니 깔끔하게 끝났다 사실 알파벳을 하나씩 보지말고 전체적으로 보고 역순보고 하면 됐다

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,49 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+
+#스택으로 풀어버렸다
+
+# n=int(input())
+# passward=[]
+
+# for i in range(n):
+#     passward.append(input())
+
+
+# stack = []
+# for i in range(n):
+#     for j in passward[i]:
+#         if j in stack:
+#             stack.remove(j)
+#             continue
+#         stack.append(j)
+#     if len(stack)==1:
+#         print(len(passward[i]),stack[0])
+#         break
+#     stack = []
+#     for k in range(i,n):
+
+#         if passward[i]==passward[k][::-1]:
+#             mid=len(passward[i])//2
+#             print(len(passward[i]),passward[i][mid])
+#             break
+
+
+n=int(input())
+passward=set()
+
+for i in range(n):
+    string=input()
+
+    if string[::-1] in passward:
+        mid=len(string)//2
+        print(len(string),string[mid])
+        break
+    passward.add(string)
+
+    if string == string[::-1]:
+        mid=len(string)//2
+        print(len(string),string[mid])
+        break
+    
+    
