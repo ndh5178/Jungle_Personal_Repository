acd4b224ea8b72ab8ac26579e07bc9e8bb1a61c5
남동현 <ehdgus5178@gmail.com>
2026-03-14 21:43:22 +0900

    백준 세수의 합 해결

--- week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
+++ week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
@@ -1,2 +1,27 @@
 # 해시 테이블 - 세 수의 합 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2295
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+
+linum=[]
+for i in range(n):
+    linum.append(int(input()))
+linum=sorted(linum)
+stlinum=sorted(linum, reverse=True)
+
+sumli = set()
+for i in range(n):
+    for j in range(i,n):
+        sumli.add(linum[i] + linum[j])
+
+for d in stlinum:
+    for c in linum:
+        if d-c in sumli:
+            print(d)
+            exit()
+
+
+
+
