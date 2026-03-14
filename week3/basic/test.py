180d2bb7c80c9c449e0ebfc0831c5a4d6a6b7ea3
남동현 <ehdgus5178@gmail.com>
2026-03-14 17:02:46 +0900

    백준 에디터 해결
    그냥 링크드리스트에 매몰되지 않기로 했다 개념은 알았으니 다음에 써먹기로

--- week3/basic/test.py
+++ week3/basic/test.py
@@ -1,3 +1,124 @@
-i=1
-i*=5
-print(i)
+# 링크드리스트 - 철도 공사 (백준 골드4)
+# 문제 링크: https://www.acmicpc.net/problem/23309
+import sys
+input = sys.stdin.readline
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head == None:
+            self.head=new_node
+            return
+        
+        lastnode=self.head
+
+        while lastnode.next != None:
+            lastnode=lastnode.next
+        lastnode.next=new_node
+
+    
+    def print_list(self):
+        values = []
+        prtnode=self.head
+        while prtnode != None:
+            values.append(prtnode.data)
+            prtnode=prtnode.next
+            if prtnode==self.head: #원형루프 종료
+                break
+        return values
+    
+    def linkedhead(self):
+        if self.head is None:
+            return
+
+        current = self.head
+
+        while current.next:
+            current = current.next
+
+        current.next = self.head
+    
+    def L(self):
+        global curser
+        if curser == 0:
+            return
+        curser-=1
+
+    def D(self):
+        global curser
+        global knum
+        if curser == knum:
+            return
+        curser+=1
+
+    def B(self):
+        global curser
+        cursernode=self.head
+        acursernode=self.head
+        for i in range(curser):
+            cursernode=cursernode.next
+        while acursernode.next !=cursernode:
+            acursernode=acursernode.next
+        savenode=acursernode
+        acursernode=acursernode.next
+        
+        if acursernode == self.head:
+            self.head = acursernode.next
+
+        acursernode=acursernode.next
+        savenode.next=acursernode
+
+    def P(self,s):
+        global curser
+        newdata=Node(s)
+        cursernode=self.head
+        for i in range(curser):
+            cursernode=cursernode.next
+        if cursernode.next == None:
+            cursernode.next=newdata
+        acursernode=cursernode.next
+        cursernode.next=newdata
+        newdata.next=acursernode
+
+
+
+li=LinkedList()
+k=input().split()
+for i in k:
+    li.append(i)
+n=int(input())
+knum=len(k)
+curser=knum
+li.linkedhead()
+li.print_list()
+for i in range(n):
+    string=input().split()
+    if len(string)==2:
+        x=string.pop()
+    if len(string)==3:
+        y=string.pop()
+        x=string.pop()    
+    z=string.pop()
+    if z=="L":
+        li.L()
+    if z=="D":
+        li.D()
+    if z=="B":
+        li.B()
+    if z=="P":
+        li.P(x)
+
+li.print_list()
+
+
+
+
+
--- week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
+++ week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
@@ -1,2 +1,48 @@
-# 링크드리스트 - 에디터 (백준 실버2)
-# 문제 링크: https://www.acmicpc.net/problem/1406
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+
+def L():
+    if len(lstack) != 0:
+        rstack.append(lstack.pop())
+    return
+
+def D():
+    if len(rstack) != 0:
+        lstack.append(rstack.pop())
+    return
+
+def B():
+    if len(lstack) != 0:
+        lstack.pop()
+    return
+def P(s):
+    lstack.append(s)
+
+s=input().rstrip()
+lstack=s[:]
+lstack=deque(lstack)
+rstack=[]
+rstack=deque(rstack)
+
+n=int(input())
+
+for i in range(n):
+    string=input().split()
+
+    if string[0]=="L":
+        L()
+    if string[0]=="D":
+        D()
+    if string[0]=="B":
+        B()
+    if string[0]=="P":
+        P(string[1])
+
+
+lstack.extend(reversed(rstack))
+result = ''.join(lstack)
+print(result)
+
