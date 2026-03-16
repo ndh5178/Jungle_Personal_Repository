5de73daa536ef08535d91db1e9fc247dc2d5c080
남동현 <ehdgus5178@gmail.com>
2026-03-16 13:49:04 +0900

    백준 탑 해결
    처음에는 투 포인터로 리스트를 뒤집어서 뒤에서부터 보고 가는데 조건에 맞으면 결과에 인덱스값을 추가하고 안 맞으면 다음 인덱스로 넘어가서 여러번 봐야했지만
    수정하고 나서는 지나간 인덱스를 저장하는 새로운 스택을 만들어서 조건에 맞으면 저장하고 조건보다 낮으면 삭제하고 해서 한번 지나간 인덱스를 다시 읽지 않게 해서 시간을 아낀거 같다 그리고 pop(0)은 앞에서 빼고 다 옮겨줘야하기 때문에 시간을 많이 잡아먹는다

--- week3/problem-solving/난이도중_스택_탑_골드5.py
+++ week3/problem-solving/난이도중_스택_탑_골드5.py
@@ -1,2 +1,97 @@
 # 스택 - 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2493
+
+# import sys
+# input = sys.stdin.readline
+
+# num=int(input())
+# arr=list(map(int,input().split()))
+# result=[]
+
+# reversarr=arr[::-1]
+
+# i=0
+# j=1
+
+# while i<len(reversarr)-1:
+#     if j==len(reversarr)-1:
+#         result.append(0)
+#         i+=1
+#         j=i+1
+#         continue
+#     if reversarr[i]<=reversarr[j]:
+#         result.append((len(reversarr)-j))
+#         i+=1
+#         j=i+1
+#         continue
+#     j+=1
+
+
+# result.append(0)
+# result=result[::-1]
+# result=list(map(str,result))
+# print(' '.join(result))
+
+###이건 생각은 좋았다고 하지만 여전히 시간 오류가 난다
+
+# while top:
+#     if len(savetop) == 0:
+#         savetop.append(top.pop(0))
+#         result.append(0)
+#     curtop = top.pop(0)
+#     vstop = savetop.pop()
+#     if curtop[0] > vstop[0]:
+#         while savetop:
+#             vstop = savetop.pop()
+#             if curtop[0] < vstop[0]:
+#                 savetop.append(vstop)
+#                 savetop.append(curtop)
+#                 result.append(vstop[1])
+#                 break
+#         if len(savetop) == 0:
+#             savetop.append(curtop)
+#             result.append(0)
+#             continue
+#     if curtop[0] <= vstop[0]:
+#         savetop.append(vstop)
+#         savetop.append(curtop)
+#         result.append(vstop[1])
+
+# enumerate
+
+import sys
+
+input = sys.stdin.readline
+
+num = int(input())
+iptop = list(map(int, input().split()))
+top = []
+savetop = []
+result = []
+
+for idx, topnum in enumerate(iptop):
+    if len(savetop) == 0:
+        savetop.append((topnum, idx + 1))
+        result.append(0)
+        continue
+    vstop = savetop.pop()
+    if topnum > vstop[0]:
+        while savetop:
+            vstop = savetop.pop()
+            if topnum < vstop[0]:
+                savetop.append(vstop)
+                savetop.append((topnum, idx + 1))
+                result.append(vstop[1])
+                break
+        if len(savetop) == 0:
+            savetop.append((topnum, idx + 1))
+            result.append(0)
+            continue
+        continue
+    if topnum <= vstop[0]:
+        savetop.append(vstop)
+        savetop.append((topnum, idx + 1))
+        result.append(vstop[1])
+result = map(str, result)
+result = " ".join(result)
+print(result)
