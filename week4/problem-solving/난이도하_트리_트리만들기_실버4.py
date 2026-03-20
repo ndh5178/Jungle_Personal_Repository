885ffa1cceebca053bc05955962521fafd39dcf1
남동현 <ehdgus5178@gmail.com>
2026-03-20 21:51:36 +0900

    백준 트리만들기 해결
    처음에 문제가 이해가 안됐지만 다시 생각하니 노드의 개수는 num이고 꼭지점이 m개인 트리를 만들라는 것이다.

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,18 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+num,mum=list(map(int,input().split()))
+mum2=1
+result=set()
+
+for i in range(num-1):
+    if i==0:
+        print(i,i+1)
+        continue
+    if mum2<mum:
+        print(1,mum2+1)
+        mum2+=1
+        continue
+    print(i,i+1)
+
+
+
