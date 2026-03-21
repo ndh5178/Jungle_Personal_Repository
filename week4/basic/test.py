0ac1862df8e8e41fec5bad36d661446d77c0fe87
남동현 <ehdgus5178@gmail.com>
2026-03-21 11:35:22 +0900

    백준 점프왕 이해
    아직 dfs를 제대로 구현하지는 못 한거 같다 생각은 했는데 방문한곳도 보드화 해야하고 리턴문안에 or을 넣는것도 생각하지 못한 방법이다

--- week4/basic/test.py
+++ week4/basic/test.py
@@ -1,14 +1,10 @@
-from collections import deque
-
-graph = {
-        0: [1, 2],
-        1: [0, 2],
-        2: [0, 1, 3],
-        3: [2]
-    }
-
-queue = deque()
-li=[]
-queue.extend(graph[0])
-print(queue)
-print(queue.popleft())
+num=int(input())
+game={
+    (0,0): 1,
+    (0,1): 2,
+    (1,0): 3
+}
+# for i in range(num):
+#     game.append(list(map(int,input().split())))
+# jjeli=0
+print(game[(0,1)])
--- week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
@@ -1,2 +1,36 @@
-# 그래프, DFS, BFS - 점프왕 쩰리 (백준 실버4)
-# 문제 링크: https://www.acmicpc.net/problem/16173
+import sys
+input = sys.stdin.readline
+
+num = int(input())
+board = []
+for i in range(num):
+    board=list(map(int,input().split()))
+
+visited = [[False] * num for i in range(num)]
+
+def dfs(x, y):
+    # 범위 밖이면 실패
+    if x < 0 or x >= num or y < 0 or y >= num:
+        return False
+
+    # 도착 지점이면 성공
+    if board[x][y] == -1:
+        return True
+
+    # 이미 방문한 곳이면 다시 안 감
+    if visited[x][y]:
+        return False
+
+    visited[x][y] = True
+
+    jump = board[x][y]
+
+    # 아래 or 오른쪽으로 이동
+    return dfs(x + jump, y) or dfs(x, y + jump)
+
+
+# 실행
+if dfs(0, 0):
+    print("HaruHaru")
+else:
+    print("Hing")
