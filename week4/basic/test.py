fec23db701d414b23145fe5e8cc49e8c23b12b6c
남동현 <ehdgus5178@gmail.com>
2026-03-21 19:35:18 +0900

    백준 이분그래프 습득
    문제는 이해가 됐지만 방문한 리스트랑 색칠 리스트까지 같이 넣는건 생각 못 할거 같다

--- week4/basic/test.py
+++ week4/basic/test.py
@@ -1,10 +1,31 @@
-num=int(input())
-game={
-    (0,0): 1,
-    (0,1): 2,
-    (1,0): 3
-}
-# for i in range(num):
-#     game.append(list(map(int,input().split())))
-# jjeli=0
-print(game[(0,1)])
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution(object):
+    def averageOfLevels(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[float]
+        """
+        result=[]
+        queue=deque()
+        queue.append(root)
+
+        while queue:
+            level = len(queue)
+            sum1 = 0
+            for i in range(level):
+                node = queue.popleft()
+                sum1 += node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            avr=float(sum1) / level
+            result.append(avr)
+        return result
+        
