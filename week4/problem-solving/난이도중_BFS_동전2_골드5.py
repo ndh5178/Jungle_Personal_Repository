86b794cb28c1b7141ce2d08b475649afbf89bd0f
남동현 <ehdgus5178@gmail.com>
2026-03-24 13:42:03 +0900

    백준 동전 해결
    똑같이 bfs써서 큐 안에 초기 동전들이랑 카운트 넣고 visited변수를 set를 줘서 만약 한번이라도 조합된 경우면 그게 최소경우일것이니 나중에 나온 경우는 고려하지 않는다 그리고 조합될수가 없을때 -1을 반환해야하는데 이게 큐를 다 쓰고 조합수가 나오는 경우도 있어서 만약 큐 안에서 조합되는수가 있다면 exit() 그러면 마지막에 굳이 조건문은 안 넣어도 되지만 불안하니 넣었다

--- week4/problem-solving/난이도중_BFS_동전2_골드5.py
+++ week4/problem-solving/난이도중_BFS_동전2_골드5.py
@@ -1,2 +1,34 @@
 # BFS - 동전 2 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2294
+import sys
+input=sys.stdin.readline
+from collections import deque
+
+num,kum=list(map(int,input().split()))
+coin=[]
+queue=deque()
+for i in range(num):
+    coin.append(int(input()))
+    queue.append((coin[i],1))
+visited=set()
+visited.add(coin[0])
+while queue:
+    value,count=queue.popleft()
+    if value == kum:
+        print(count)
+        exit()
+    for i in coin:
+        savevalue=0
+        savevalue=value+i
+        if savevalue == kum:
+            print(count+1)
+            exit()
+        if savevalue>kum:
+            continue
+        if savevalue not in visited:
+            queue.append((savevalue,count+1))
+            visited.add(savevalue)
+    if savevalue == kum:
+        break
+if not queue:
+    print(-1)
