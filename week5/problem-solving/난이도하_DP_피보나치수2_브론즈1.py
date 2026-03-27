a7fb9edacf2c925938ce51ea0dbab789d632a16e
남동현 <ehdgus5178@gmail.com>
2026-03-27 13:10:20 +0900

    백준피보나치 수 2 해결

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,22 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+import sys
+input=sys.stdin.readline
+
+num=int(input())
+
+def fib(n,memo=None):
+    if memo is None:
+        memo={}
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    if n in memo:
+        return memo[n]
+    
+    memo[n]=fib(n-1,memo)+fib(n-2,memo)
+    return memo[n]
+
+print(fib(num))
