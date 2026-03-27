bbf9690c44d2e1d640b389a72d0cfbcb5127b006
남동현 <ehdgus5178@gmail.com>
2026-03-27 15:13:51 +0900

    백준 동전 0 해결
    그리디 동전 거스름돈이랑 같은 경우인거 같다

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,15 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+num,kum=list(map(int,input().split()))
+coins=[]
+for i in range(num):
+    coins.append(int(input()))
+coins=coins[::-1]
+count=0
+for i in coins:
+    momey=kum//i
+    if momey>0:
+        count+=momey
+        kum%=i
+print(count)
