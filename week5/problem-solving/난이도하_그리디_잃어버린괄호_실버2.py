e275c0ce7204bb8163a965a9b4cb6455595e37c0
남동현 <ehdgus5178@gmail.com>
2026-03-27 16:33:21 +0900

    백준 잃어버린 괄호 해결
    이걸 그리디를 사용했는지는 아직 모르겠다 그냥 내가 생각한 알고리즘대로 풀었는데 맞은느낌?

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,28 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+import sys
+input=sys.stdin.readline
+
+num=list(map(str,input().split("-")))
+intnum=''
+result=0
+
+selected=0
+if '+' in num[0]:
+    num[0]=num[0].split('+')
+    for i in num[0]:
+        result+=int(i)
+else:
+    result+=int(num[0])
+selected=result
+
+for i in range(1,len(num)):
+    result=0
+    if '+' in num[i]:
+        num[i]=num[i].split('+')
+        for j in num[i]:
+            result+=int(j)
+    else:
+        result+=int(num[i])
+    selected-=result
+print(selected)
