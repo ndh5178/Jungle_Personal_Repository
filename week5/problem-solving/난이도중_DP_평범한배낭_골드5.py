0163e064c6320cf89ffa4277499ebdb56009f63e
남동현 <ehdgus5178@gmail.com>
2026-03-28 15:17:10 +0900

    백준 배낭 해결
    처음에는 무게보다 가치를 우선으로 판단하려했는데 그것보다 무게우선으로 해야하고 무게도 더 무거운거 부터 내려가야했다 이걸 평범하게 생각할수있을까?

--- week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
+++ week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
@@ -1,2 +1,24 @@
 # DP - 평범한 배낭 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/12865
+import sys
+input=sys.stdin.readline
+
+num,kum=list(map(int,input().split()))
+bag=[]
+for i in range(num):
+    wum,vum=list(map(int,input().split()))
+    bag.append((wum,vum))
+bag.sort(key=lambda x:-x[0])
+
+dp=[]
+for i in range(kum + 1):
+    dp.append(0)
+
+for i in range(num):
+    weight, value = bag[i]
+
+    for j in range(kum, weight - 1, -1):
+        if dp[j] < dp[j - weight] + value:
+            dp[j] = dp[j - weight] + value
+
+print(dp[kum])
