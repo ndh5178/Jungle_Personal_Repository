4682394e5da2c698687b2b1e956ad87534b7e20b
남동현 <ehdgus5178@gmail.com>
2026-03-28 13:53:50 +0900

    백준 동전 해결
    드디어 dp개념을 조금 알거같다
    먼저 초기값 설정하고 바텀 업 방식으로 1원으로 1000원을 만드는데 과정이 1원으로 1원 만들기 2원만들기 이걸 각각 저장하고 끝나면 2원으로 2원 만드는 경우 3원 만드는 경우 4원 만드는 경우 이때 4원은 2원을 만드는 경우에서 추가인거니 더해주는것이다

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,25 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+import sys
+input=sys.stdin.readline
+
+tum=int(input())
+
+for i in range(tum):
+    num=int(input())
+    coins=list(map(int,input().split()))
+    money=int(input())
+    dp=[]
+    for j in range(money + 1):
+        dp.append(0)
+    dp[0]=1
+
+    for coin in coins:
+        for j in range(coin, money + 1):
+            dp[j] += dp[j - coin]
+    
+    print(dp[money])
+
+
+
+
