1800482f6a36927f19eb4a3fd15d53bc2f0b39a4
남동현 <ehdgus5178@gmail.com>
2026-03-28 23:34:52 +0900

    백준 신입사원 해결
    처음에는 서류 최고점 면접 최고점으로 양쪽으로 비교해서 사람을 뽑았는데 그게 아니라 서류최고점으로 정렬하고 다음에 면접을 비교해서 기준점인 사람보다 순위가 더 낮은 사람이면
    알아서 탈락 아니면 사람교체

--- week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -1,2 +1,43 @@
 # 그리디 - 신입 사원 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1946
+
+import sys
+input=sys.stdin.readline
+
+aum=int(input())
+for _ in range(aum):
+    rank=[]
+    bum=int(input())
+    for i in range(bum):
+        rank.append(list(map(int,input().split())))
+    if bum == 1:
+        print(bum)
+        continue
+    if bum == 2:
+        if [1,1] in rank:
+            print(1)
+            continue
+        print(2)
+        continue
+    rank.sort()
+    result=[]
+    suru=rank[0]
+    munjup=rank[1]
+    for i in range(2,len(rank)):
+        change=rank[i]
+        if suru[0]>change[0]:
+            save=change
+            change=suru
+            suru=save
+        if munjup[1]>change[1]:
+            save=change
+            change=munjup
+            munjup=save
+        if suru[0]<change[0] and suru[1]<change[1]:
+            continue
+        if munjup[0]<change[0] and munjup[1]<change[1]:
+            continue
+        result.append(change)
+    
+    print(len(result)+2)
+            
