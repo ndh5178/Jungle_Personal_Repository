6a90cb0e835a912adea7edffe781451baa4e9dd2
남동현 <ehdgus5178@gmail.com>
2026-03-29 17:37:57 +0900

    백준 멀티탭스케줄링 학습
    나는 각각 사용하는 콘센트의 사용횟수만큼 우선순위를 주고 그 우선순위를 보고 판단해 플로그를 뽑고 그걸 카운트 한다고 했는데 우선순위랑 뒤에있는 플러그까지 봐야하기 때문에 틀린거같다

--- week5/problem-solving/난이도상_그리디_멀티탭스케줄링_골드1.py
+++ week5/problem-solving/난이도상_그리디_멀티탭스케줄링_골드1.py
@@ -1,2 +1,95 @@
 # 그리디 - 멀티탭 스케줄링 (백준 골드1)
 # 문제 링크: https://www.acmicpc.net/problem/1700
+# import sys
+# input = sys.stdin.readline
+
+# aum,bum=list(map(int,input().split()))
+# multitab={}
+# for i in range(bum):
+#     multitab[i+1]=0
+# cum=list(map(int,input().split()))
+# save=0
+# for i in cum:
+#     if save == i:
+#         continue
+#     multitab[i]+=1
+#     save=i
+# plug=[]
+# for i in cum:
+#     if save == i:
+#         continue
+#     plug.append([multitab[i],i])
+#     multitab[i]-=1
+#     save=i
+
+# first=[]
+# first2=[]
+# count=0
+# i=0
+# while i<aum:
+#     if cum[i] in first2:
+#         aum+=1
+#         i+=1
+#         continue
+#     first2.append(cum[i])
+#     first.append(plug[i])
+#     i+=1
+# for i in range(aum,len(plug)):
+#     a,b=plug[i]
+#     for j in range(len(first)):
+#         a1,b1=first[j]
+#         if b == b1:
+#             first[j]=plug[i]
+#             break
+#     if b == b1:
+#         continue
+#     first.sort()
+#     first[0]=plug[i]
+#     count+=1
+    
+# print(count)
+
+
+import sys
+input = sys.stdin.readline
+
+aum, bum = map(int, input().split())
+cum = list(map(int, input().split()))
+
+plug = []
+count = 0
+
+for i in range(bum):
+    now = cum[i]
+
+    if now in plug:
+        continue
+
+    if len(plug) < aum:
+        plug.append(now)
+        continue
+
+    remove_index = -1
+    farthest = -1
+
+    for j in range(len(plug)):
+        device = plug[j]
+
+        next_use = -1
+        for k in range(i + 1, bum):
+            if cum[k] == device:
+                next_use = k
+                break
+
+        if next_use == -1:
+            remove_index = j
+            break
+
+        if next_use > farthest:
+            farthest = next_use
+            remove_index = j
+
+    plug[remove_index] = now
+    count += 1
+
+print(count)
--- week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -1,43 +1,70 @@
 # 그리디 - 신입 사원 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1946
 
-import sys
-input=sys.stdin.readline
+# import sys
+# input=sys.stdin.readline
 
-aum=int(input())
-for _ in range(aum):
-    rank=[]
-    bum=int(input())
-    for i in range(bum):
-        rank.append(list(map(int,input().split())))
-    if bum == 1:
-        print(bum)
-        continue
-    if bum == 2:
-        if [1,1] in rank:
-            print(1)
-            continue
-        print(2)
-        continue
-    rank.sort()
-    result=[]
-    suru=rank[0]
-    munjup=rank[1]
-    for i in range(2,len(rank)):
-        change=rank[i]
-        if suru[0]>change[0]:
-            save=change
-            change=suru
-            suru=save
-        if munjup[1]>change[1]:
-            save=change
-            change=munjup
-            munjup=save
-        if suru[0]<change[0] and suru[1]<change[1]:
-            continue
-        if munjup[0]<change[0] and munjup[1]<change[1]:
-            continue
-        result.append(change)
+# aum=int(input())
+# for _ in range(aum):
+#     rank=[]
+#     bum=int(input())
+#     for i in range(bum):
+#         rank.append(list(map(int,input().split())))
+#     if bum == 1:
+#         print(bum)
+#         continue
+#     if bum == 2:
+#         if [1,1] in rank:
+#             print(1)
+#             continue
+#         print(2)
+#         continue
+#     rank.sort()
+#     result=[]
+#     suru=rank[0]
+#     munjup=rank[1]
+#     for i in range(2,len(rank)):
+#         change=rank[i]
+#         if suru[0]>change[0]:
+#             save=change
+#             change=suru
+#             suru=save
+#         if munjup[1]>change[1]:
+#             save=change
+#             change=munjup
+#             munjup=save
+#         if suru[0]<change[0] and suru[1]<change[1]:
+#             continue
+#         if munjup[0]<change[0] and munjup[1]<change[1]:
+#             continue
+#         result.append(change)
     
-    print(len(result)+2)
+#     print(len(result)+2)
+
+
+
             
+import sys
+input = sys.stdin.readline
+
+aum = int(input())
+
+for _ in range(aum):
+    bum = int(input())
+    rank = []
+
+    for i in range(bum):
+        paper, interview = map(int, input().split())
+        rank.append((paper, interview))
+
+    rank.sort()
+
+    count = 1
+    best_interview = rank[0][1]
+
+    for i in range(1, bum):
+        if rank[i][1] < best_interview:
+            count += 1
+            best_interview = rank[i][1]
+
+    print(count)
