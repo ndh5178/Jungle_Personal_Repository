5c413f1d3ee52cdc1d1e1a57c33235386fe92437
남동현 <ehdgus5178@gmail.com>
2026-04-03 11:45:42 +0900

    6주차 공부내용 복사

--- week5/basic/test.py
+++ week5/basic/test.py
@@ -26,28 +26,63 @@
 # print(sol.wordBreak("applepenapple",["apple","pen"]))
 
 class Solution(object):
-    def wordBreak(self, s, wordDict):
+    def minimumTotal(self, triangle):
         """
-        :type s: str
-        :type wordDict: List[str]
-        :rtype: bool
+        :type triangle: List[List[int]]
+        :rtype: int
         """
-        dp={}
-        for i in range(len(s)+1):
-            dp[i]=0
-        dp[0]=1
-        for i in range(1,len(s)+1):
-            if s[0:i] in wordDict:
-                dp[i]=1
-                start=i
-        for j in range(start,len(s)+1):
-            if s[start:j] in wordDict:
-                dp[i]=1
-                start=i
-        if dp[len(s)]==1:
-            return True
-        else:
-            return False
+        # dp={}
+        # dp[0]=(triangle[0][0],0)
+
+        # for i in range(1,len(triangle)):
+        #     x,y=dp[i-1]
+        #     if triangle[i][y]<triangle[i][y+1]:
+        #         dp[i]=(x+triangle[i][y],y)
+        #     else:
+        #         dp[i]=(x+triangle[i][y+1],y+1)
+        # return dp[len(triangle)-1][0]
+        
+        # def dfs(graph, start, visited=None):
+        #     """
+        #     깊이 우선 탐색 (재귀)
+            
+        #     Args:
+        #         graph: 그래프 딕셔너리
+        #         start: 현재 정점
+        #         visited: 방문 리스트
+            
+        #     Returns:
+        #         방문 순서 리스트
+        #     """
+        #     # TODO: visited가 None이면 초기화
+        #     if visited == None:
+        #         visited={}
+        #     pass
+            
+        #     # TODO: 현재 정점 방문
+        #     visited.append(start)
+        #     pass
+            
+        #     # TODO: 인접한 정점들에 대해 재귀
+        #     ## 방문하지 않은 정점이면 재귀 호출
+        #     for i in graph[start]:
+        #         if i not in visited:
+        #             dfs(graph,i,visited)
+        #     pass
+            
+        #     return visited
+        # graph={}
+        # for i in range(len(triangle)):
+        #     for j in triangle[i]:
+        #         if not graph[i]:
+        #             graph[i]=[]
+        #         if not graph[j]:
+        #             graph[j]=[]
+        #         graph[i].append(j)
+        #         graph[j].append(i)
+        # print(graph)
+        # 
 
 sol=Solution()
-print(sol.wordBreak("catsandog",["cats","dog","sand","and","cat"]))
+triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+print(sol.minimumTotal(triangle))
