2d5732a15eedaed3562445ee66360f9bbf2b2774
남동현 <ehdgus5178@gmail.com>
2026-04-16 13:43:22 +0900

    8주차 학습내용 교체

--- C언어/webproxy_lab_docker/webproxy-lab/tiny/tiny.c
+++ C언어/webproxy_lab_docker/webproxy-lab/tiny/tiny.c
@@ -17,27 +17,30 @@ void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
                  char *longmsg);
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   int listenfd, connfd;
   char hostname[MAXLINE], port[MAXLINE];
   socklen_t clientlen;
   struct sockaddr_storage clientaddr;
 
   /* Check command line args */
-  if (argc != 2) {
+  if (argc != 2)
+  {
     fprintf(stderr, "usage: %s <port>\n", argv[0]);
     exit(1);
   }
 
   listenfd = Open_listenfd(argv[1]);
-  while (1) {
+  while (1)
+  {
     clientlen = sizeof(clientaddr);
     connfd = Accept(listenfd, (SA *)&clientaddr,
-                    &clientlen);  // line:netp:tiny:accept
+                    &clientlen); // line:netp:tiny:accept
     Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
                 0);
     printf("Accepted connection from (%s, %s)\n", hostname, port);
-    doit(connfd);   // line:netp:tiny:doit
-    Close(connfd);  // line:netp:tiny:close
+    doit(connfd);  // line:netp:tiny:doit
+    Close(connfd); // line:netp:tiny:close
   }
 }
